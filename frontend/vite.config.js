dac7cf14fe67305fcbe12ef420c747ffe3e869fe
dong8824 <ldz532926676@gmail.com>
2026-01-04 21:11:32 +0800

    Fix Vercel build script and output dir

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,10 @@ import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   css: {
     postcss: {
       plugins: [
